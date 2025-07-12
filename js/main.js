d2df30193b6bd9f908612dd54a9a5e46dcf3439b
idle5go4 <idle5go4@gmail.com>
2025-07-12 19:15:25 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,11 @@
-async function loadVideos() {
-  const res = await fetch('./data/mv_special.json');
-  const videos = await res.json();
-  const container = document.getElementById('video-container');
-  videos.sort((a, b) => new Date(b.date) - new Date(a.date));
-  videos.forEach(video => {
-    const div = document.createElement('div');
-    div.className = 'video-card';
-    div.innerHTML = `
-      <img src="./assets/thumbnails/${video.thumbnail}" alt="${video.title}">
-      <h3>${video.title}</h3>
-      <p>${video.date}</p>
-      <a href="https://youtu.be/${video.youtubeId}" target="_blank">영상 보기</a>
-    `;
-    container.appendChild(div);
-  });
-}
-loadVideos();
+import { loadCards, sortCards, renderCards } from "./modules/cardManager.js";
+import { setupSearch } from "./modules/searchHandler.js";
+import { setupSortToggle } from "./modules/sortToggle.js";
+import { setupScrollButtons } from "./modules/scroll.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadCards();
+  setupSearch();
+  setupSortToggle();
+  setupScrollButtons();
+});
